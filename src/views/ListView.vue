a43dbc9f6d9a78fba26ee565aa1463cf19747bcc
vxgganxv <vxgganxv@naver.com>
2019-05-22 21:29:17 +0900

    upate

---
+++ src/views/ListView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="cont list">
+		<list-item></list-item>
+	</div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+
+export default {
+	components: {
+		ListItem,
+	},
+	// created() {
+	// 	this.$emit('on:progress');
+	// 	this.$store.dispatch('FETCH_LIST', this.$route.name)
+	// 		.them(() => this.$emit('off:progress'))
+	// 		.catch(() => console.log('fail'));
+	// }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/listview.scss';
+</style>
